ba0489723d40ee4d4d9259db0d2cb2001b12fbca
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-04-22 13:58:12 -0600

    Use parameterized test

--- src/test/java/com/bmuschko/ApplicationIntegrationTest.java
+++ src/test/java/com/bmuschko/ApplicationIntegrationTest.java
@@ -3,7 +3,8 @@ package com.bmuschko;
 import jdk.incubator.http.HttpClient;
 import jdk.incubator.http.HttpRequest;
 import jdk.incubator.http.HttpResponse;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 import java.net.URI;
@@ -11,15 +12,15 @@ import java.net.URI;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationIntegrationTest {
-    @Test
-    void canResolveApplicationUrl() throws IOException, InterruptedException {
+    @ParameterizedTest(name = "can resolve application URL {0} times and receive correct count")
+    @ValueSource(ints = { 1, 2, 3 })
+    void canResolveApplicationUrl(int times) throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://localhost:5000/"))
                 .GET()
                 .build();
         HttpResponse<String> response = HttpClient.newHttpClient()
                 .send(request, HttpResponse.BodyHandler.asString());
-        String responseBody = response.body();
-        assertTrue(responseBody.startsWith("Welcome to this awesome page!"));
+        assertTrue(response.body().contains(String.format("Welcome to this awesome page! You've visited me %s times.", times)));
     }
 }
