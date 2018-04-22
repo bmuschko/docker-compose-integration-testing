3ce5d5798c627252867bcdcd00c39dc6fe616ef7
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-04-22 09:25:36 -0600

    Better assertions

--- src/test/java/com/bmuschko/ApplicationIntegrationTest.java
+++ src/test/java/com/bmuschko/ApplicationIntegrationTest.java
@@ -3,6 +3,7 @@ package com.bmuschko;
 import jdk.incubator.http.HttpClient;
 import jdk.incubator.http.HttpRequest;
 import jdk.incubator.http.HttpResponse;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -11,15 +12,24 @@ import java.net.URI;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationIntegrationTest {
-    @Test
-    void canResolveApplicationUrl() throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
+    private static HttpClient httpClient;
+    private static HttpRequest request;
+
+    @BeforeAll
+    static void setup() {
+        httpClient = HttpClient.newHttpClient();
+        request = HttpRequest.newBuilder()
                 .uri(URI.create("http://localhost:5000/"))
                 .GET()
                 .build();
-        HttpResponse<String> response = HttpClient.newHttpClient()
-                .send(request, HttpResponse.BodyHandler.asString());
-        String responseBody = response.body();
-        assertTrue(responseBody.startsWith("Welcome to this awesome page!"));
+    }
+
+    @Test
+    void canResolveApplicationUrl() throws IOException, InterruptedException {
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandler.asString());
+        assertTrue(response.body().contains("Welcome to this awesome page! You've visited me 1 times."));
+
+        response = httpClient.send(request, HttpResponse.BodyHandler.asString());
+        assertTrue(response.body().contains("Welcome to this awesome page! You've visited me 2 times."));
     }
 }
