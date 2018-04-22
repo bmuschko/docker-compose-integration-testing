7742c2b06f17426652e29dc9e9fc69c16e93613c
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-04-22 07:58:25 -0600

    Optimize imports

--- src/test/java/com/bmuschko/ApplicationIntegrationTest.java
+++ src/test/java/com/bmuschko/ApplicationIntegrationTest.java
@@ -1,14 +1,13 @@
 package com.bmuschko;
 
-import java.io.IOException;
-import java.lang.InterruptedException;
-import java.net.URI;
 import jdk.incubator.http.HttpClient;
 import jdk.incubator.http.HttpRequest;
 import jdk.incubator.http.HttpResponse;
-import jdk.incubator.http.HttpResponse.BodyHandler;
-
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationIntegrationTest {
